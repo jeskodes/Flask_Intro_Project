6b4816b2fc9028dc2d8351d29108bec231bd464d
Jessica Clark <jesclark28@gmail.com>
2023-12-22 07:36:29 +0000

    Add about member.html and route to py

--- app.py
+++ app.py
@@ -21,6 +21,16 @@ def about():
     return render_template("about.html", page_title="About", company=data) 
     # Creating new variable company which is loading json_data with command json.load.
 
+@app.route("/about/<member_name>")
+def about_member(member_name):
+    member = {}
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+        for obj in data:
+            if obj["url"] == member_name: 
+                member = obj
+    return render_template("member.html", member=member)
+
 @app.route("/careers")
 def careers(): 
     return render_template("careers.html", page_title="Come Work with Us")
